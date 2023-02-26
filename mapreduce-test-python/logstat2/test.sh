b015c999acd4477453b7cb99678b70b0b0d714ce
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-02-26 18:36:40 -0500

    Update test.sh

--- mapreduce-test-python/logstat2/test.sh
+++ mapreduce-test-python/logstat2/test.sh
@@ -5,9 +5,9 @@
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat2/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat2/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/logstat2/mapper.py -mapper ../../mapreduce-test-python/logstat2/mapper.py \
--file ../../mapreduce-test-python/logstat2/reducer.py -reducer ../../mapreduce-test-python/logstat2/reducer.py  \
--input /logstat2/input/* -output /logstat2/output/
+-file mapper.py -mapper mapper.py \
+-file reducer.py -reducer reducer.py \
+-cmdenv RANGE="${1}" \
 /usr/local/hadoop/bin/hdfs dfs -cat /logstat2/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
