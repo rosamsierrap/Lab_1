6d27db97ff77de3277e5ca5f14fe1e7286a818b3
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-02-26 15:24:54 -0500

    Update mapper.py

--- mapreduce-test-python/logstat2/mapper.py
+++ mapreduce-test-python/logstat2/mapper.py
@@ -4,7 +4,8 @@ import re
 import os
 import sys
 
-I_hour, F_hour = os.environ['range'].strip().split("-")
+I_hour, F_hour = 12-13
+#os.environ['range'].strip().split("-")
 #the user inputs the hour range #12,12 
 I_hour, F_hour = int(I_hour), int(F_hour)
 
