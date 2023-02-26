f454fda55e1e0f7eda2f7326ed35fa3e1d3d5776
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-02-26 16:21:25 -0500

    Update mapper.py

--- mapreduce-test-python/logstat2/mapper.py
+++ mapreduce-test-python/logstat2/mapper.py
@@ -4,24 +4,23 @@ import re
 import os
 import sys
 
-I_hour = $initial
-F_hour = 13
-#os.environ['range'].strip().split("-")
-#the user inputs the hour range #12,12 
-I_hour, F_hour = int(I_hour), int(F_hour)
+def mapper(hour):
+    I_hour , F_hour = os.environ['range'].strip().split("-")
+    #the user inputs the hour range #12,12 
+    I_hour, F_hour = int(I_hour), int(F_hour)
 
-if I_hour > 23 or F_hour >23 or I_hour<0 or F_hour<0:
-    #errors in the hours
-    #print('Invalid input')
-    raise Exception('Invalid Input')
+    if I_hour > 23 or F_hour >23 or I_hour<0 or F_hour<0:
+        #errors in the hours
+        #print('Invalid input')
+        raise Exception('Invalid Input')
 
-pat = re.compile('(?P<ip>\d+\.\d+\.\d+\.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
-for line in sys.stdin:
-    match = pat.search(line)
-    if match:
+    pat = re.compile('(?P<ip>\d+\.\d+\.\d+\.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
+    for line in sys.stdin:
+        match = pat.search(line)
+        if match:
 
-        hour = int(match.group('hour'))
+            hour = int(match.group('hour'))
 
-        if hour >= I_hour and hour <= F_hour:
-            #only consider the visits inside this hour range
-            print('{}\t{}'.format('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1))
+            if hour >= I_hour and hour <= F_hour:
+                #only consider the visits inside this hour range
+                print('{}\t{}'.format('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1))
