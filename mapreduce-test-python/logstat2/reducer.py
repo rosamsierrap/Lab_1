8799e3b921c81f2cfe4432fb4608ae9ad30b2131
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-02-26 14:21:25 -0500

    Update reducer.py

--- mapreduce-test-python/logstat2/reducer.py
+++ mapreduce-test-python/logstat2/reducer.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
+# --*-- coding:utf-8 --*-
 from operator import itemgetter
 import sys
 
-dict_ip_count = {}
+dictionary_ip_count = {}
 
 for line in sys.stdin:
     line = line.strip()
-    ip, num = line.split('\t')
+    ip, num = line.split()
+    #here ip is ip + hour, now we extract only the ip address:
+    ip_address = ip[7:]
+
     try:
         num = int(num)
-        dict_ip_count[ip] = dict_ip_count.get(ip, 0) + num
+        dictionary_ip_count[ip_address] = dictionary_ip_count.get(ip_address, 0) + num
+        # adding the values of each ip search in the hour range
 
     except ValueError:
         pass
 
-
-sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
-for ip, count in sorted_dict_ip_count:
-    print '%s\t%s' % (ip, count)
+sorted_dictionary_ip_count = sorted(dictionary_ip_count.items(), key=itemgetter(1), reverse=True)
+#sort reserved = the max to minimun count
+for ip_address, count in sorted_dictionary_ip_count:
+    print ('{}\t{}'.format(ip_address, count)) #print the top 3 addresses in the range
+               
