a3a37e3d8901c72bd6b6b6c01ea1d11fcf7ffc85
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-02-26 19:00:48 -0500

    Update mapper.py

--- mapreduce-test-python/logstat2/mapper.py
+++ mapreduce-test-python/logstat2/mapper.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python
 # --*-- coding:utf-8 --*--
-def mapper(arg):
-    import re
-    import os
-    import sys
 
-    I_hour , F_hour = arg.strip().split("-")
-    #os.environ['RANGE'].strip().split("-")
-    #the user inputs the hour range #12,12 
-    I_hour, F_hour = int(I_hour), int(F_hour)
+import re
+import os
+import sys
 
-    if I_hour > 23 or F_hour >23 or I_hour<0 or F_hour<0:
-        #errors in the hours
-        #print('Invalid input')
-        raise Exception('Invalid Input')
+I_hour , F_hour = os.environ['arg'].strip().split("-")
+#os.environ['RANGE'].strip().split("-")
+#the user inputs the hour range #12,12 
+I_hour, F_hour = int(I_hour), int(F_hour)
 
-    pat = re.compile('(?P<ip>\d+\.\d+\.\d+\.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
-    for line in sys.stdin:
-        match = pat.search(line)
-        if match:
+if I_hour > 23 or F_hour >23 or I_hour<0 or F_hour<0:
+    #errors in the hours
+    #print('Invalid input')
+    raise Exception('Invalid Input')
 
-            hour = int(match.group('hour'))
+pat = re.compile('(?P<ip>\d+\.\d+\.\d+\.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
+for line in sys.stdin:
+    match = pat.search(line)
+    if match:
 
-            if hour >= I_hour and hour <= F_hour:
-                #only consider the visits inside this hour range
-                print('{}\t{}'.format('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1))
+        hour = int(match.group('hour'))
+
+        if hour >= I_hour and hour <= F_hour:
+            #only consider the visits inside this hour range
+            print('{}\t{}'.format('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1))
